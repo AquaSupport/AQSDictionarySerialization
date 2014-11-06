b2c97f94a1855a4b7f4bffdd5a552a9b9dca3396
Kai INUI <lied.der.optik@gmail.com>
2014-11-06 20:36:45 +0900

    Init

---
+++ AQSDictionarySerialization/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  AQSDictionarySerialization
+//
+//  Created by kaiinui on 2014/11/06.
+//  Copyright (c) 2014年 Aquamarine. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ AQSDictionarySerialization/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  AQSDictionarySerialization
+//
+//  Created by kaiinui on 2014/11/06.
+//  Copyright (c) 2014年 Aquamarine. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
